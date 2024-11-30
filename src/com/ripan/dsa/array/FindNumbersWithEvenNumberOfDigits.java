fea0b42c3e34b07f2224622d1f8a09f01d068f42
ripan baidya <ripanbaidya7980@gmail.com>
2024-12-01 01:31:38 +0530

    leetcode:1295, find numbers with even numbers of digits

---
+++ src/com/ripan/dsa/array/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,34 @@
+package com.ripan.dsa.array;
+
+// leetcode 1925
+public class FindNumbersWithEvenNumberOfDigits {
+
+    public static boolean isEvenNumberDigits(int num){
+        int cnt = 0; // count the number of digits
+        while(num != 0){
+            int rem = num % 10;
+            cnt ++;
+            num /= 10;
+        }
+
+        // check whether the count is even or not
+        return (cnt % 2 == 0);
+    }
+
+    public static int findNumbers(int[] nums) {
+        int cnt = 0; // count the number, having even number of digits
+
+        for(int i = 0 ;i < nums.length; i ++){
+            if(isEvenNumberDigits(nums[i]))
+                cnt ++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896}; // Output: 2
+
+        int result = findNumbers(nums);
+        System.out.println(result);
+    }
+}
