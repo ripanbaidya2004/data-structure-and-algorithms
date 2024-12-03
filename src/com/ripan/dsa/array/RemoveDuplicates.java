701d0bb68e7e68009e6861785d3445e974bb2b98
ripan baidya <ripanbaidya7980@gmail.com>
2024-12-03 10:43:09 +0530

    leetcode:26, Remove duplicates from sorted array

--- src/com/ripan/dsa/array/RemoveDuplicates.java
+++ src/com/ripan/dsa/array/RemoveDuplicates.java
@@ -5,15 +5,21 @@ package com.ripan.dsa.array;
 public class RemoveDuplicates {
 
     private static int removeDuplicated(int[] arr){
-       int i = 0 ;
-       for(int j = 1 ; j <= arr.length-1 ; j ++){
+       int i = 0 ; // the first element in an array as itself is a unique element.
+
+        /**
+         * start iterating from the second element in the array,
+         * I am looking for the element which is unique.
+         */
+       for(int j = 1 ; j < arr.length ; j ++){
            if(arr[i] != arr[j]){
-               arr[i+1] = arr[j];
-               i ++;
+               // whatever my first element was, if it is different from its next element
+               i ++; // increment the count, to give the position of next element just after the first element
+               arr[i] = arr[j];
            }
        }
        // time: O(n), space: O(1)
-       return (i+1);
+       return (i+1); // since 0 based indexing, Increment +1 while returning the answer
     }
 
     public static void main(String[] args) {
