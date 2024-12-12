c3bed2920f4d6ddde65f25dec8c277f6b8f0cb86
ripan baidya <ripanbaidya7980@gmail.com>
2024-12-12 12:35:23 +0530

    leetcode 278, First Bad Version

---
+++ src/com/ripan/dsa/binarysearch/FirstBadVersion.java
@@ -0,0 +1,29 @@
+package com.ripan.dsa.binarysearch;
+// leetcode 278
+public class FirstBadVersion {
+
+    /**
+     *  The isBadVersion API is defined in the parent class VersionControl.
+     *  boolean isBadVersion(int version);
+     *  */
+    /**
+     * public class Solution extends VersionControl {
+     *     public int firstBadVersion(int n) {
+     *
+     *         int l = 1;
+     *         int r = n;
+     *
+     *         while(l < r){
+     *             int mid = l + (r - l)/2;
+     *
+     *             if(isBadVersion(mid)){
+     *                 r = mid;
+     *             } else {
+     *                 l = mid + 1;
+     *             }
+     *         }
+     *         return r;
+     *     }
+     * }
+     */
+}
