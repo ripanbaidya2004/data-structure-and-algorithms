3a6e398cdb2b745f1a49c4077ddb853f6bb6bfc9
ripan baidya <ripanbaidya7980@gmail.com>
2024-12-14 11:16:04 +0530

    coding ninja, Upper Bound

---
+++ src/com/ripan/dsa/binarysearch/UpperBound.java
@@ -0,0 +1,28 @@
+package com.ripan.dsa.binarysearch;
+
+public class UpperBound {
+    public static int upperBound(int []arr, int n, int x) {
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+
+        // lower bound: The smallest index, such that arr[idx] >= x
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,8,8,10};
+        int target = 8;
+
+        System.out.println(upperBound(arr, arr.length, target));
+    }
+}
